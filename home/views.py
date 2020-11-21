27fb2cb608cc6cc7f057e990945485023226127f
Saharalnoor <baiarrah_04@yahoo.com>
2020-11-21 21:40:42 +0000

    Fixed products view and based.html  Added product_details and Faqs folder and paths

--- home/views.py
+++ home/views.py
@@ -7,3 +7,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def faqs(request):
+    """A view to return the faqs page"""
+
+    return render(request, 'home/faqs.html')
