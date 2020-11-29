8548ccb79f5e2ec79d6297b0b7397369d79bbc06
Saharalnoor <baiarrah_04@yahoo.com>
2020-11-29 23:35:14 +0000

    Added and styled about page

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name="about"),
     path('faqs/', views.faqs, name="faqs"),
     path('contact_us/', views.contact_us, name="contact_us")
 ]
--- home/views.py
+++ home/views.py
@@ -11,6 +11,12 @@ def index(request):
     return render(request, 'home/index.html')
 
 
+def about(request):
+    """A view to return the about page"""
+
+    return render(request, 'home/about.html')
+
+
 def faqs(request):
     """A view to return the faqs page"""
 
