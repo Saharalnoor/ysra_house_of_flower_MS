d7bb305cfd7ed45c1fc47b22b95da48919da8119
Saharalnoor <baiarrah_04@yahoo.com>
2020-11-29 14:37:00 +0000

    Added basic payment funtionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -27,3 +27,51 @@ const style = {
 
 let card = elements.create('card', {hidePostalCode: true, style: style});
 card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function (event) {
+    const errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-exclamation-circle"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+    }
+  }).then(function(result) {
+    if (result.error) {
+      const errorDiv = document.getElementById('card-errors');
+                let html = `
+                    <span class="icon" role="alert">
+                        <i class="fas fa-exclamation-circle"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('.spinner-overlay-wrapper').fadeToggle(1200);
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+    } else {
+        if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+    }
+  });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render, redirect, reverse
 from .forms import OrderForm
+from cart.contexts import cart_contents
+from django.conf import settings
+from django.contrib import messages
+
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.error(request, 'Stripe public key is missing.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Hsd8OK4QpIUZwG9napIA3Ya6WVBXSlmaOstBTAhTsoCpDgDXtMSO9br3dvOwfC89eREdWqkVfNp5ibX9LqJSXCD00uDEeFPB1'
-        'client_secret' 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
