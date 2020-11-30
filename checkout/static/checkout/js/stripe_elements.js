3efae8366b8a4ccb690241698a79f2712dec77a4
Saharalnoor <baiarrah_04@yahoo.com>
2020-11-30 12:20:19 +0000

    Trying to solve checkout bugs

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,22 +1,22 @@
-  
-/**
- *  Core logic/payment flow for this comes from Stripe documentations:
- *  https://stripe.com/docs/payments/accept-a-payment
- *  with minor modification and additions
- */
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
 
-const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-let clientSecret = $('#id_client_secret').text().slice(1, -1);
-let stripe = Stripe(stripePublicKey);
-let elements = stripe.elements();
-const style = {
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
     base: {
         color: '#000',
-        fontFamily: '"Helvetica", sans-serif',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
         '::placeholder': {
-            color: '#495057'
+            color: '#aab7c4'
         }
     },
     invalid: {
@@ -24,17 +24,16 @@ const style = {
         iconColor: '#dc3545'
     }
 };
-
-let card = elements.create('card', {hidePostalCode: true, style: style});
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-// Handle real-time validation errors from the card Element.
+// Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
-    const errorDiv = document.getElementById('card-errors');
+    var errorDiv = document.getElementById('card-errors');
     if (event.error) {
-        let html = `
+        var html = `
             <span class="icon" role="alert">
-                <i class="fas fa-exclamation-circle"></i>
+                <i class="fas fa-times"></i>
             </span>
             <span>${event.error.message}</span>
         `;
@@ -51,27 +50,69 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-      card: card,
-    }
-  }).then(function(result) {
-    if (result.error) {
-      const errorDiv = document.getElementById('card-errors');
-                let html = `
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From using {% csrf_token %} in the form
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function () {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address:{
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
+            },
+        }).then(function(result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
                     <span class="icon" role="alert">
-                        <i class="fas fa-exclamation-circle"></i>
+                    <i class="fas fa-times"></i>
                     </span>
                     <span>${result.error.message}</span>`;
                 $(errorDiv).html(html);
                 $('#payment-form').fadeToggle(100);
-                $('.spinner-overlay-wrapper').fadeToggle(1200);
+                $('#loading-overlay').fadeToggle(100);
                 card.update({ 'disabled': false});
                 $('#submit-button').attr('disabled', false);
-    } else {
-        if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                   form.submit();
+                }
             }
-    }
-  });
+        });
+    }).fail(function () {
+        // just reload the page, the error will be in django messages
+        location.reload();
+    })
 });
