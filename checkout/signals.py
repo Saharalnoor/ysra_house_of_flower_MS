14026c59ce3abfb3c94a1a33af6028bc9e587a97
Saharalnoor <baiarrah_04@yahoo.com>
2020-11-30 16:17:07 +0000

    Initial Deployment

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from .models import OrderLineItem
 
--- ysra_house_of_roses/settings.py
+++ ysra_house_of_roses/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    'profiles',
 
     'crispy_forms',
 ]
@@ -118,12 +120,17 @@ WSGI_APPLICATION = 'ysra_house_of_roses.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
