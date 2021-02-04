f0e73322f3773c8d2aa417cd7a83cba06488bd40
Saharalnoor <baiarrah_04@yahoo.com>
2021-02-04 18:39:32 +0000

    Added contact_thankyou.html path

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name="about"),
     path('faqs/', views.faqs, name="faqs"),
-    path('contact_us/', views.contact_us, name="contact_us")
+    path('contact_us/', views.contact_us, name="contact_us"),
+    path('success/', views.contact_thankyou, name='contact_thankyou')
 ]
