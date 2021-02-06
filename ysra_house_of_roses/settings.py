8cb3ed59ad4a17e8ee031de6e26f0400815298e8
Saharalnoor <baiarrah_04@yahoo.com>
2021-02-06 10:21:44 +0000

    Updated Allow Host

--- ysra_house_of_roses/settings.py
+++ ysra_house_of_roses/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'ysra-house-of-roses.herokuapp.com']
+ALLOWED_HOSTS = ['ysra-house-of-roses.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
